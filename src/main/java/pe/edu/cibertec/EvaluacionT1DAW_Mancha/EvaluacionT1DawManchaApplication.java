17bfc58e2f917cb41562a5c5eec5c8d86182800c
elvisniltonmc <nilton_leo@hotmail.com>
2024-09-25 13:25:06 -0500

    ExamenT1NiltonMancha

---
+++ src/main/java/pe/edu/cibertec/EvaluacionT1DAW_Mancha/EvaluacionT1DawManchaApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.EvaluacionT1DAW_Mancha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvaluacionT1DawManchaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvaluacionT1DawManchaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/EvaluacionT1DAW_Mancha/EvaluacionT1DawManchaApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.EvaluacionT1DAW_Mancha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvaluacionT1DawManchaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
