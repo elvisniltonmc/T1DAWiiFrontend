f10b000a647440828aae99f3cc0011dc01358b64
elvisniltonmc <nilton_leo@hotmail.com>
2024-09-27 15:33:35 -0500

    Acabando la parte fronEnd del examen 15:33 pm

--- src/main/java/pe/edu/cibertec/EvaluacionT1DAW_Mancha/controller/LoginControllerT1.java
+++ src/main/java/pe/edu/cibertec/EvaluacionT1DAW_Mancha/controller/LoginControllerT1.java
@@ -28,24 +28,24 @@ public class LoginControllerT1 {
             Model model) {
 
         try {
-            // Validar campos de entrada
+
             if (tipoDocumento == null || tipoDocumento.trim().length() == 0 ||
                     NumeroDocumento == null || NumeroDocumento.trim().length() == 0 ||
                     Password == null || Password.trim().length() == 0) {
 
-                LoginModel loginModel = new LoginModel("01", "Error: Debe completar bien sus credenciales", "Cesar Santos");
+                LoginModel loginModel = new LoginModel("01", "Error: Debe completar bien sus credenciales", "Son goku");
                 model.addAttribute("loginmodel", loginModel);
-                return "iniciando";  // Asegúrate de que la vista 'iniciando' existe
+                return "iniciando";
             }
 
             // Si la validación es exitosa
             LoginModel loginModel = new LoginModel("00", "", "Cesar Santos");
             model.addAttribute("loginmodel", loginModel);
-            return "home";  // Asegúrate de que la vista 'home' existe
+            return "home";
         } catch (Exception e) {
             // Manejo del error
             model.addAttribute("errorMessage", "Ocurrió un error inesperado: " + e.getMessage());
-            return "error";  // Retorna a una página de error personalizada
+            return "error";
         }
     }
 
